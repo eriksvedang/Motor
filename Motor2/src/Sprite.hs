c28c8d8412f35f77a0546ee063c638018235e985
Erik <erik.svedang@gmail.com>
2015-01-04 17:25:46 +0100

    quad

--- Motor2/src/Sprite.hs
+++ Motor2/src/Sprite.hs
@@ -9,15 +9,24 @@ import Foreign.Storable (sizeOf)
 import Linear
 import System.FilePath ((</>))
 
-vertexBufferData3, vertexBufferData4 :: [GLfloat]
-vertexBufferData3  = [-0.1, 0, 0, 0.1, 0.1, 0]
-vertexBufferData4 = [-0.5, 0, 0, -1, 1, 0]
+spriteQuad :: [GLfloat]
+spriteQuad  = [-1, -1,
+               -1,  1,
+                1,  1,
+                1, -1,
+               -1, -1]
 
 spriteExampleSetup = do
+  Right rur <- readTexture ("resources" </> "Rur.png")
+
+  textureFilter   Texture2D   $= ((Linear', Nothing), Linear')
+  textureWrapMode Texture2D S $= (Mirrored, ClampToEdge)
+  textureWrapMode Texture2D T $= (Mirrored, ClampToEdge)
+
   prog <- simpleShaderProgram ("resources" </> "shape.v.glsl") ("resources" </> "shape.f.glsl")
   currentProgram $= Just (program prog)
-  a <- makeBuffer ArrayBuffer vertexBufferData3
-  b <- makeBuffer ArrayBuffer vertexBufferData4
+  a <- makeBuffer ArrayBuffer spriteQuad
+  b <- makeBuffer ArrayBuffer spriteQuad
   let stride = fromIntegral $ sizeOf (undefined::GLfloat) * 2
       vad = VertexArrayDescriptor 2 Float stride offset0
   return (prog, vad, a, b)
@@ -27,6 +36,6 @@ spriteExampleRender (prog, vad, a, b) = do
   enableAttrib prog "coord2d"
   setAttrib prog "coord2d" ToFloat vad
   setUniform prog "col" (V3 (0::GLfloat) 1 0)
-  setUniform prog "m_transform" (move (-0.2) 0.0)
-  drawArrays Triangles 0 3
+  setUniform prog "m_transform" (move 0 0)
+  drawArrays TriangleStrip 0 6
 
