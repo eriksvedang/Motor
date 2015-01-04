d61235c1e25e7f336aa55675f14b3213b651b5ac
Erik <erik.svedang@gmail.com>
2015-01-04 19:50:34 +0100

    FINALLY, I CAN CHANGE TEXTURE!!!!!!!

--- Motor2/resources/sprite.f.glsl
+++ Motor2/resources/sprite.f.glsl
@@ -1,11 +1,20 @@
 #version 110
 
-uniform sampler2D texture0;
+uniform sampler2D tex;
+uniform vec3 col;
+
 varying vec2 texcoord;
 
 void main()
 {
-  gl_FragColor = texture2D(texture0, texcoord);
+  gl_FragColor = texture2D(tex, texcoord);
+  
+  //gl_FragColor = vec4(col, 1.0);
+
+  // gl_FragColor = mix(
+  //       texture2D(textures[0], texcoord),
+  //       texture2D(textures[1], texcoord),
+  //       0.5);
 }
 
 // uniform vec3 col;
--- Motor2/src/Sprite.hs
+++ Motor2/src/Sprite.hs
@@ -8,6 +8,7 @@ import qualified Graphics.UI.GLFW as GLFW
 import Foreign.Storable (sizeOf)
 import Linear
 import System.FilePath ((</>))
+import Control.Applicative
 
 spriteQuad :: [GLfloat]
 spriteQuad  = [-1, -1,
@@ -16,21 +17,14 @@ spriteQuad  = [-1, -1,
                 1, -1,
                -1, -1]
 
+loadTex :: FilePath -> IO TextureObject
+loadTex f = do t <- either error id <$> readTexture f
+               textureFilter Texture2D $= ((Linear', Nothing), Linear')
+               texture2DWrap $= (Repeated, ClampToEdge)
+               return t
+
 spriteExampleSetup = do
-  Right rur <- readTexture ("resources" </> "Rur.png")
-  Right jur <- readTexture ("resources" </> "Jur.png")
-
-  --textureFilter Texture2D $= ((Linear', Nothing), Linear')
-  textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
-
-  --textureWrapMode Texture2D S $= (Mirrored, ClampToEdge)
-  --textureWrapMode Texture2D T $= (Mirrored, ClampToEdge)
-  textureWrapMode Texture2D S $= (Repeated, Repeat)
-  textureWrapMode Texture2D T $= (Repeated, Repeat)
-
-  --activeTexture $= TextureUnit 0
-  textureBinding Texture2D $= Just rur
-
+  
   blend $= Enabled
   blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
 
@@ -39,17 +33,41 @@ spriteExampleSetup = do
   quad <- makeBuffer ArrayBuffer spriteQuad
   let stride = fromIntegral $ sizeOf (undefined::GLfloat) * 2
       vad = VertexArrayDescriptor 2 Float stride offset0
+
+  
+  rur <- loadTex ("resources" </> "Rur.png")
+  jur <- loadTex ("resources" </> "Jur.png")
+  lur <- loadTex ("resources" </> "Lur.png")
+
+  putStrLn $ "rur: " ++ show rur
+  putStrLn $ "jur: " ++ show jur
+  putStrLn $ "lur: " ++ show lur
+  
+  --textureFilter Texture2D $= ((Linear', Nothing), Linear')
+  textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
+
+  --textureWrapMode Texture2D S $= (Mirrored, ClampToEdge)
+  --textureWrapMode Texture2D T $= (Mirrored, ClampToEdge)
+  textureWrapMode Texture2D S $= (Repeated, Repeat)
+  textureWrapMode Texture2D T $= (Repeated, Repeat)
+
+  texture Texture2D $= Enabled
+  activeTexture $= TextureUnit 0
+  textureBinding Texture2D $= Just rur
+
+  activeTexture $= TextureUnit 1
+  textureBinding Texture2D $= Just jur
       
-  return (prog, vad, quad)
+  return (prog, vad, quad, rur, jur)
 
-spriteExampleRender (prog, vad, quad) = do
+spriteExampleRender (prog, vad, quad, rur, jur) = do
   --drawAt 0 0
-  drawAt (prog, vad, quad) 0.2 0
+  drawAt (prog, vad, quad, rur, jur) 0.2 0
 
-drawAt (prog, vad, quad) x y = do
+drawAt (prog, vad, quad, rur, jur) x y = do
   bindBuffer ArrayBuffer $= Just quad
   enableAttrib prog "coord2d"
   setAttrib prog "coord2d" ToFloat vad
   setUniform prog "m_transform" (move x y)
-  --setUniform prog "texture0" (TextureUnit 0)
+  setUniform prog "tex" $ TextureUnit 1
   drawArrays TriangleStrip 0 6
