4c4083f3ca8eb98091fd3d4f85d2346fcf995717
Erik <erik.svedang@gmail.com>
2015-01-04 20:19:08 +0100

    scaling kinda works too

--- Motor2/resources/sprite.v.glsl
+++ Motor2/resources/sprite.v.glsl
@@ -4,8 +4,9 @@ attribute vec2 coord2d;
 varying vec2 texcoord;
 
 uniform mat4 m_transform;
+uniform mat4 m_scale;
 
 void main(void) {
-  gl_Position = m_transform * vec4(coord2d, 0.0, 1.0);
+  gl_Position = m_transform * m_scale * vec4(coord2d, 0.0, 1.0);
   texcoord = vec2(coord2d.x, -coord2d.y) * vec2(0.5) + vec2(0.5);
 }
--- Motor2/src/Sprite.hs
+++ Motor2/src/Sprite.hs
@@ -7,19 +7,21 @@ import Graphics.GLUtil
 import qualified Graphics.UI.GLFW as GLFW
 import Foreign.Storable (sizeOf)
 import Linear
+import Linear.Matrix
 import System.FilePath ((</>))
 import Control.Applicative
 
 spriteQuad :: [GLfloat]
-spriteQuad  = [-1, -1,
-               -1,  1,
-                1,  1,
-                1, -1,
-               -1, -1]
+spriteQuad  = [-1.0, -1.0,
+               -1.0,  1.0,
+                1.0,  1.0,
+                1.0, -1.0,
+               -1.0, -1.0]
 
 loadTex :: FilePath -> IO TextureObject
 loadTex f = do t <- either error id <$> readTexture f
-               textureFilter Texture2D $= ((Linear', Nothing), Linear')
+               --textureFilter Texture2D $= ((Linear', Nothing), Linear')
+               textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
                texture2DWrap $= (Repeated, ClampToEdge)
                return t
 
@@ -34,40 +36,36 @@ spriteExampleSetup = do
   let stride = fromIntegral $ sizeOf (undefined::GLfloat) * 2
       vad = VertexArrayDescriptor 2 Float stride offset0
 
-  
   rur <- loadTex ("resources" </> "Rur.png")
   jur <- loadTex ("resources" </> "Jur.png")
   lur <- loadTex ("resources" </> "Lur.png")
 
-  putStrLn $ "rur: " ++ show rur
-  putStrLn $ "jur: " ++ show jur
-  putStrLn $ "lur: " ++ show lur
-  
-  --textureFilter Texture2D $= ((Linear', Nothing), Linear')
-  textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
-
-  --textureWrapMode Texture2D S $= (Mirrored, ClampToEdge)
-  --textureWrapMode Texture2D T $= (Mirrored, ClampToEdge)
-  textureWrapMode Texture2D S $= (Repeated, Repeat)
-  textureWrapMode Texture2D T $= (Repeated, Repeat)
-
   texture Texture2D $= Enabled
+  
   activeTexture $= TextureUnit 0
   textureBinding Texture2D $= Just rur
 
   activeTexture $= TextureUnit 1
   textureBinding Texture2D $= Just jur
       
-  return (prog, vad, quad, rur, jur)
+  return (prog, vad, quad)
 
-spriteExampleRender (prog, vad, quad, rur, jur) = do
-  --drawAt 0 0
-  drawAt (prog, vad, quad, rur, jur) 0.2 0
+spriteExampleRender (prog, vad, quad) = do
+  drawAt (prog, vad, quad) 0 (-0.75) 0.3
+  drawAt (prog, vad, quad) 1 0.9 (-0.2)
 
-drawAt (prog, vad, quad, rur, jur) x y = do
+drawAt (prog, vad, quad) n x y = do
   bindBuffer ArrayBuffer $= Just quad
   enableAttrib prog "coord2d"
   setAttrib prog "coord2d" ToFloat vad
   setUniform prog "m_transform" (move x y)
-  setUniform prog "tex" $ TextureUnit 1
+  setUniform prog "m_scale" (scaling 0.25)
+  setUniform prog "tex" $ TextureUnit n
   drawArrays TriangleStrip 0 6
+
+scaling :: GLfloat -> M44 GLfloat
+scaling s =
+  V4 (V4 s 0 0 0)
+     (V4 0 s 0 0)
+     (V4 0 0 s 0)
+     (V4 0 0 0 1)
