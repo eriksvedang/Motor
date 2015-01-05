6dbb13803de26d307c5309207f216c875b912574
Erik <erik.svedang@gmail.com>
2015-01-05 17:17:32 +0100

    cleanup!

--- Motor2/src/Sprite.hs
+++ Motor2/src/Sprite.hs
@@ -24,30 +24,31 @@ spriteQuad  = [-1.0, -1.0,
 loadTex :: String -> IO TextureObject
 loadTex name = do
   tex <- either error id <$> readTexture ("resources" </> name)
-  --textureFilter Texture2D $= ((Linear', Nothing), Linear')
   texture2DWrap $= (Repeated, ClampToEdge)
   textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
-  --putStrLn $ "Bound " ++ name ++ ", " ++ show tex ++ " to texture unit " ++ show textureUnitNr
+  --textureFilter Texture2D $= ((Linear', Nothing), Linear')
   return tex
 
 data SpriteStore a = SpriteStore {
   prog :: ShaderProgram,
   vad :: VertexArrayDescriptor a,
   quad :: BufferObject,
-  textureUnits :: [(String, CUInt)]
+  textureNamesToUnits :: [(String, CUInt)]
 }
 
-assignTextureToNr texture nr = do
-  activeTexture $= TextureUnit nr
+assignTextureToUnit :: TextureObject -> CUInt -> IO ()
+assignTextureToUnit texture unitNr = do
+  activeTexture $= TextureUnit unitNr
   textureBinding Texture2D $= Just texture
 
 getTextureUnit :: SpriteStore a -> String -> TextureUnit
-getTextureUnit store name =
-  case lookup name (textureUnits store) of
-   Nothing -> error $ "Failed to find texture with name " ++ name ++ " in SpriteStore"
+getTextureUnit store textureName =
+  case lookup textureName (textureNamesToUnits store) of
+   Nothing -> error $ "Failed to find texture with name " ++ textureName ++ " in SpriteStore"
    Just n -> TextureUnit n
 
-mkSpriteStore spriteNames = do
+mkSpriteStore :: [String] -> IO (SpriteStore a)
+mkSpriteStore textureNames = do
   prog <- simpleShaderProgram ("resources" </> "sprite.v.glsl") ("resources" </> "sprite.f.glsl")
   currentProgram $= Just (program prog)
   quad <- makeBuffer ArrayBuffer spriteQuad
@@ -57,31 +58,18 @@ mkSpriteStore spriteNames = do
         prog = prog,
         vad = vad,
         quad = quad,
-        textureUnits = zipWith (,) spriteNames [0..]
+        textureNamesToUnits = zipWith (,) textureNames [0..]
       }
 
-  textures <- mapM loadTex spriteNames
-  zipWithM_ assignTextureToNr textures [0..]
+  textures <- mapM loadTex textureNames
+  zipWithM_ assignTextureToUnit textures [0..]
 
   return store
-
-spriteExampleSetup = do
-  store <- mkSpriteStore ["Rur.png", "Jur.png", "Lur.png"]
-  return store
-
-spriteExampleRender window store = do
-  drawAt store window "Rur.png" 0 0
-  drawAt store window "Lur.png" 2 0
-  drawAt store window "Jur.png" 4 0
 
 camera winW winH = Linear.ortho (-w) (w) (-h) (h) 0 1
          where w = (winW / spriteSize)::GLfloat
                h = (winH / spriteSize)::GLfloat
                spriteSize = 64
-
---cam :: Camera GLfloat
---cam = roll 45 $ camera2D
--- (m33_to_m44 $ camMatrix cam)
 
 drawAt store window textureName x y = do
   bindBuffer ArrayBuffer $= Just (quad store)
@@ -93,4 +81,13 @@ drawAt store window textureName x y = do
   setUniform (prog store) "m_cam" (camera (fromIntegral winW) (fromIntegral winH))
   setUniform (prog store) "tex" $ (getTextureUnit store textureName)
   drawArrays TriangleStrip 0 6
+
+spriteExampleSetup = do
+  store <- mkSpriteStore ["Rur.png", "Jur.png", "Lur.png"]
+  return store
+
+spriteExampleRender window store = do
+  drawAt store window "Rur.png" 0 0
+  drawAt store window "Lur.png" 2 0
+  drawAt store window "Jur.png" 4 0
 
